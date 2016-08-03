41d51acaf18d9adb01a85ccf70f9a9472bb42716
Arthur <artcslee@gmail.com>
2016-08-03 01:42:48 -0700

    attempt trim off of coin change

--- src/com/arthur/problems/CoinChange.java
+++ src/com/arthur/problems/CoinChange.java
@@ -27,11 +27,11 @@ public class CoinChange extends LeetCodeProblem {
     public void solution(Object arg) {
         //Setup Problem
         List<Integer> args = (List<Integer>) arg;
-        int target = args.get(0);
+        int amount = args.get(0);
         List<Integer> coins = args.subList(1, args.size());
 
         //Print Problem
-        System.out.print("Target: [" + target + "], Coins: [");
+        System.out.print("Target: [" + amount + "], Coins: [");
         for (int x=0; x<coins.size(); x++) {
             System.out.print(coins.get(x));
             if (x < coins.size()-1) {
@@ -42,47 +42,40 @@ public class CoinChange extends LeetCodeProblem {
         }
 
         //Solve Problem
-        HashMap<Integer, Integer> smallestSet = getSmallestSet(coins, target);
-        int totalCoinNum = 0;
-        if (smallestSet == null) {
-            totalCoinNum = -1;
-        } else {
-            totalCoinNum = getNumOfCoins(smallestSet, true);
-        }
+        int totalCoinNum = getLeastCoinNum(coins, amount, Integer.MAX_VALUE);
         System.out.println("Total Num Of Coins: [" + totalCoinNum + "]");
     }
 
-    public HashMap<Integer, Integer> getSmallestSet(List<Integer> coins, int target) {
-        int coin = coins.get(0);
-        HashMap <Integer, Integer> smallestSet = null;
-        if(coins.size() == 1) {
-            if(target % coin == 0) {
-                smallestSet = new HashMap<>();
-                int numOfLastCoin = target/coin;
-                smallestSet.put(coin, numOfLastCoin);
-            }
-            return smallestSet;
+    public int getLeastCoinNum(List<Integer> coins, int target, int leastNum) {
+        if (coins == null || coins.size() < 1) {
+            return -1;
         }
 
+        int coin = coins.get(0);
+        int leastCoinNum = -1;
+
+        //base case
+        if(coins.size() == 1) {
+            if(target % coin == 0) {
+                leastCoinNum = target/coin;
+            }
+            return leastCoinNum;
+        }
+
+        //recursive case
         int currNum = 0;
         while (currNum * coin <= target) {
-            HashMap<Integer, Integer> othersSet = getSmallestSet(coins.subList(1, coins.size()), target - (currNum * coin));
-            if (othersSet != null) {
-                if (smallestSet == null) {
-                    smallestSet = new HashMap<>();
-                }
-
-                int numOfCoins = currNum + getNumOfCoins(othersSet, false);
-                int currSmallestNum = getNumOfCoins(smallestSet, false);
-                if (smallestSet.isEmpty() || numOfCoins < currSmallestNum) {
-                    smallestSet.put(coin, currNum);
-                    smallestSet.putAll(othersSet);
+            int leastSubsetCoinNum = getLeastCoinNum(coins.subList(1, coins.size()), target - (currNum * coin), leastCoinNum);
+            int numOfCoins = currNum + leastSubsetCoinNum;
+            if (leastSubsetCoinNum >= 0) {
+                if (leastCoinNum < 0 || numOfCoins < leastCoinNum) {
+                    leastCoinNum = numOfCoins;
                 }
             }
             currNum++;
         }
 
-        return smallestSet;
+        return leastCoinNum;
     }
 
     private int getNumOfCoins(HashMap<Integer, Integer> coinSet, boolean debug) {
