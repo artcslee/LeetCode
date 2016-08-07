3c1b3c9f54d8c436826b9619e0be4f40a8e67b4e
artcslee <skqldi2@>
2016-08-07 15:26:28 -0700

    Finish longest palindrome

--- src/com/arthur/problems/LongestPalindrome.java
+++ src/com/arthur/problems/LongestPalindrome.java
@@ -2,6 +2,8 @@ package com.arthur.problems;
 
 import com.arthur.LeetCodeProblem;
 
+import java.util.HashMap;
+
 /**
  * Created by artcslee on 8/4/16.
  */
@@ -19,70 +21,117 @@ public class LongestPalindrome extends LeetCodeProblem {
         System.out.println("Longest Palindrome: [" + palindrome + "]");
     }
 
+    String checkString;
+    String longestPalindrome;
+
+    private static final int ODD = 1;
+    private static final int EVEN = 0;
+
     public String longestPalindrome(String s) {
-        String palindrome = null;
         if (s.length() == 1) {
             return s;
         }
+        checkString = s;
         int midPoint = s.length()/2;
         int indexOffset = 0;
+        String currPalindrome = longestPalindrome;
 
-        while((midPoint - indexOffset) > 0 && (palindrome == null || !checkLongest(s, palindrome, indexOffset))) {
-            //EXPlORE LEFT
+        while((midPoint + indexOffset) < s.length()) {
             System.out.println("--------------------------");
-            String palHalf = s.substring(0, midPoint-indexOffset);
-            System.out.println("LEFT: [" + palHalf + "] ");
-            String rString = reverseString(palHalf);
-            String foundPalindrome = fetchPalindrome(s, midPoint-indexOffset, 0, rString);
-            palindrome = foundPalindrome == null ? palindrome : foundPalindrome;
+
+            //EXPlORE LEFT
+            int palIndex = midPoint-indexOffset;
+            String palString = s.substring(0, palIndex);
+            String revString = reverseString(palString);
+
+            System.out.println("LEFT: [" + palString + "]");
+            if (revString.length()%2 == 0) {
+                getPalindrome(palIndex, revString, EVEN);
+            } else {
+                getPalindrome(palIndex, revString, ODD);
+            }
 
             //EXPLORE RIGHT
-            if (indexOffset > 0 && !checkLongest(s, palindrome, indexOffset)) {
-                int palIndex = midPoint + indexOffset;
+            if (indexOffset > 0) {
+                palIndex = midPoint + indexOffset;
                 int rightLength = s.length() - palIndex - 1;
-                palHalf = s.substring(palIndex-rightLength, palIndex);
-                System.out.println("RIGHT: [" + s.substring(palIndex+1, s.length()) + "]");
-                rString = reverseString(palHalf);
-                int currPalLength = palindrome == null ? 0 : palindrome.length();
-                foundPalindrome = fetchPalindrome(s, midPoint+indexOffset, currPalLength, rString);
-                palindrome = foundPalindrome == null ? palindrome : foundPalindrome;
+                palString = s.substring(palIndex - rightLength, palIndex);
+                revString = reverseString(palString);
+                System.out.println("RIGHT[" + palString + "]");
+                if (revString.length()%2 == 0) {
+                    getPalindrome(palIndex, revString, EVEN);
+                } else {
+                    getPalindrome(palIndex, revString, ODD);
+                }
             }
 
             indexOffset++;
             System.out.println("--------------------------");
         }
 
-        return palindrome;
+        return longestPalindrome;
     }
 
-    public String fetchPalindrome(String s, int index, int currLongest, String revLString) {
-        String palindrome = null;
-
+    public String getPalindrome(int index, String revLString, int isOdd) {
+        if (longestPalindrome!=null && longestPalindrome.length() > maxPotential(revLString)) return longestPalindrome;
         if (revLString.length() == 0) {
-            if (s.charAt(index) == s.charAt(index-1)) {
-                palindrome = s.substring(index-1, index+1);
-            } else if ((index+1<s.length()) && (s.charAt(index) == s.charAt(index+1))) {
-                palindrome = s.substring(index, index+2);
+            if (longestPalindrome == null && index-1 >= 0) {
+                System.out.println("[" + checkString.charAt(index-1) + "][" + checkString.charAt(index) + "]");
+                if(checkString.charAt(index) == checkString.charAt(index-1)) {
+                    longestPalindrome = checkString.substring(index - 1, index + 1);
+                }
             }
-            return palindrome;
+            if (longestPalindrome == null && index+1 < checkString.length()) {
+                System.out.println("[" + checkString.charAt(index) + "][" + checkString.charAt(index+1) + "]");
+                if (checkString.charAt(index) == checkString.charAt(index+1)) {
+                    longestPalindrome = checkString.substring(index, index + 2);
+                }
+            }
+            return longestPalindrome;
         }
 
-        int palIndex = index+1;
-        if (s.length() < palIndex + revLString.length()) {
-            return fetchPalindrome(s, index, currLongest, revLString.substring(0, revLString.length()-1));
+        //Create Left String
+        String lString = revLString;
+        while (index+lString.length()+isOdd > checkString.length()) {
+            lString = lString.substring(0, lString.length()-1);
+        }
+        if (lString.length() == 0) return getPalindrome(index, lString, Math.abs(isOdd));
+
+        //Create Right String
+        String rString = checkString.substring(index+isOdd, index+lString.length()+isOdd);
+
+        //Print Comparing
+        if (isOdd == ODD) {
+            System.out.println("[" + lString + "][" + checkString.charAt(index) + "][" + rString + "]");
+        } else {
+            System.out.println("[" + lString + "][" + rString + "]");
         }
 
-    String rString = s.substring(palIndex, palIndex+revLString.length());
-    System.out.println("[" + revLString + "][" + s.charAt(index) + "][" + rString + "]");
-    if (revLString.equals(rString)) {
-        palindrome = s.substring(index-revLString.length(), palIndex + revLString.length());
-    } else {
-        if (currLongest < revLString.length()) {
-            palindrome = fetchPalindrome(s, index, currLongest, revLString.substring(0, revLString.length()-1));
+        //Compare
+        String currPalindrome = null;
+        if (lString.equals(rString)) {
+            currPalindrome = checkString.substring(index-lString.length(), index+rString.length()+isOdd);
+            updateLongestPalindrome(currPalindrome);
+        }
+
+        if (currPalindrome == null || currPalindrome.length() < maxPotential(revLString)) {
+            String longerCurrPalindrome = getPalindrome(index, lString.substring(0, lString.length() - isOdd), Math.abs(isOdd - 1));
+            updateLongestPalindrome(longerCurrPalindrome);
+        }
+
+        return longestPalindrome;
+    }
+
+    public void updateLongestPalindrome(String foundPalindrome) {
+        if (foundPalindrome == null) return;
+        if (longestPalindrome == null || longestPalindrome.length() < foundPalindrome.length()) {
+            longestPalindrome = foundPalindrome;
         }
     }
-    return palindrome;
-}
+
+    public int maxPotential(String lString) {
+        return (lString.length()*2) + 1;
+    }
 
     public String reverseString(String s) {
         String reverse = "";
@@ -90,12 +139,5 @@ public class LongestPalindrome extends LeetCodeProblem {
             reverse += s.charAt(i);
         }
         return reverse;
-    }
-
-    public boolean checkLongest(String s, String palindrome, int offSet) {
-        if (palindrome == null || palindrome.length()/2 < s.length()/2-offSet) {
-            return false;
-        }
-        return true;
     }
 }
